f9cd04934f16a38b1d208b7c152f84a5df47640f
Matt Peschke <2604242+mpeschke@users.noreply.github.com>
2022-10-14 21:34:04 +0200

    1) Removed cgram copy instruction from main_lcd1602 example to decrease hex file size - close to its 4K flash size 2) Added Makefile with build steps into creating a HEX file to be flashed into a AT89S51 using avrdude

--- lcd1602_at89s51/main_lcd1602.c
+++ lcd1602_at89s51/main_lcd1602.c
@@ -3,15 +3,11 @@
 
 void main() {
     int i = 32767;
-    uint8_t cgdata1[] = {0x04, 0x0A, 0x11, 0x0A, 0x04, 0x04, 0x04, 0x04};
 
     disp_start(2,16);
 
-    lcd_put_cg_addr(0x00);
-    lcd_cpy_cgram(cgdata1, 8);
-
     disp_put_cur(0,0);
-    disp_printf("Hello world!");
+    disp_printf("THIS IS LINE 01");
     disp_put_cur(1,0);
     disp_printf("Int test: %d", i);
     disp_cur_on();
